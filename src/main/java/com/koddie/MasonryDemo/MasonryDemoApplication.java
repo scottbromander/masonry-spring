8488e9e89476fcb66699e9e408c3b0d3203e8c2d
Scott Bromander <scott@primeacademy.io>
2018-03-29 10:56:53 -0700

    initial commit

---
+++ src/main/java/com/koddie/MasonryDemo/MasonryDemoApplication.java
@@ -0,0 +1,12 @@
+package com.koddie.MasonryDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MasonryDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MasonryDemoApplication.class, args);
+	}
+}
---
+++ src/test/java/com/koddie/MasonryDemo/MasonryDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.koddie.MasonryDemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MasonryDemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
